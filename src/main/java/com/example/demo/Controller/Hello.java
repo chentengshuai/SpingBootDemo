7d0f24effad8b686704fb5a3f2bd33b52ac15fd9
chentengshuai <729027995@qq.com>
2019-03-21 17:58:18 +0800

    使用redis初例

--- src/main/java/com/example/demo/Controller/Hello.java
+++ src/main/java/com/example/demo/Controller/Hello.java
@@ -11,18 +11,18 @@
 package com.example.demo.Controller;
 
 import com.example.common.SayHelloByName;
-import com.example.common.SpringContextUtil;
 import com.example.demo.utils.Test;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
-import org.jboss.logging.Param;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import redis.clients.jedis.Jedis;
 
 @Api(value = "API - HelloController", description = "用户测试的Hello类")
 @RestController
@@ -31,6 +31,15 @@ public class Hello {
     @Autowired
     private Test test;
 
+    @Value("${redis.host}")
+    String host;
+    @Value("${redis.port}")
+    int port;
+    @Value("${redis.password}")
+    String password;
+    @Autowired
+    private SayHelloByName sayHelloByName;
+
     /**
     * @Description:   返回HelloChenTengShuai
     * @Param:
@@ -38,16 +47,6 @@ public class Hello {
     * @Author: 陈腾帅
     * @Date: 2019-03-20
     */
-//    @RequestMapping("/SayHelloChenTengShuai")
-//    public String SayHelloChenTengShuai() {
-//        SayHelloByName sayHelloByName = SpringContextUtil.getBean("sayHelloByName",SayHelloByName.class);
-//        return sayHelloByName.sayHelloByName("ChenTengShuai");
-//    }
-
-
-    @Autowired
-    private SayHelloByName sayHelloByName;
-
     @RequestMapping(value = "/SayHelloAutowired",method= RequestMethod.GET)
     public String SayHelloChenTengShuai(@RequestParam(value="name", required=false, defaultValue="ChenTengShuai")String name) {
         return sayHelloByName.sayHelloByName(name);
@@ -81,6 +80,35 @@ public class Hello {
     @RequestMapping(value = "SayHelloByName",method= RequestMethod.GET)
     public String sayHelloByName(@RequestParam(value="name", required=false, defaultValue="ChenTengShuai")String name) {
         String result = test.sayHelloByName(name);
+        return result;
+    }
+
+    /**
+     * @Description: 使用redis缓存数据初例
+     * @Param: 需要保存的数据
+     * @return:
+     * @Author: 陈腾帅
+     * @Date: 2019-03-21
+     */
+    @RequestMapping(value = "useRedisIndex", method = RequestMethod.GET)
+    public String useRedisIndex() {
+        String result = "";
+        try {
+            //使用前确保redis服务已经开启
+            //通过地址和端口号进行认证
+            Jedis jedis = new Jedis(host, port);
+            //存在密码时使用密码认证
+            jedis.auth(password);
+            jedis.set("name", "张三");
+            jedis.set("age", "18");
+            String s = jedis.get("name");
+            String s1 = jedis.get("age");
+            System.out.println(s);
+            System.out.println(s1);
+            result = "成功！";
+        } catch (Exception e) {
+            result = "失败：" + e.toString();
+        }
         return result;
     }
 }
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,8 +3,10 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+//@ImportResource(value = {"classpath:applicationContext.xml"})
 @ComponentScan("com.example.*")
 public class DemoApplication {
 
